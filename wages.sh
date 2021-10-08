db0d000426beb9e104a72c7efeddad35bc5716a5
Nicholas Ross <nross3@nd.edu>
2021-10-07 21:18:53 -0400

    Edits to shell code

--- wages.sh
+++ wages.sh
@@ -22,14 +22,24 @@ echo "The number of females in the top 10 earners: $topFemales"
 
 
 #3rd code will set two variables equal to a pipeline that will identify the lines in which individuals have 12 and 16 years of education,
-#and will find the difference between the average wages of both groups. Finally, this difference will be printed
+#and will find the difference between the minimum and average wages of both groups. Finally, this difference will be printed
 
-#usage: variable=$(cut -d delimiter -f columns_to_keep filename | sed -n '/line_starting_with_number_to_keep/p' | 
-#cut -d delimiter -f column_to_keep |awk '{variable_name+=sprintf("%number_type", $column_number)} END{printf "%number_type\newline", variable/NR}') 
-val16=$(cut -d "," -f 3,4 $1 | sed -n '/^16/p'| sed 's/,/ /g'| cut -d " " -f 2| awk '{sum+=sprintf("%f",$1)}END{printf "%.6f\n",sum/NR}')
-val12=$(cut -d "," -f 3,4 $1 | sed -n '/^12/p'| sed 's/,/ /g'| cut -d " " -f 2| awk '{sum+=sprintf("%f",$1)}END{printf "%.6f\n",sum/NR}')
+#usage: variable=$(cut -d delimiter -f columns_to_keep filename|sed -n '/keepline_starting_with_number/p' |cut -d delimiter -f column_to_keep |
+#sort -typeofsort | head -lines_from_top_to_pull
+min16=$(cut -d "," -f 3,4 $1 | sed -n '/^16/p'| cut -d "," -f 2| sort -n| head -1)
+min12=$(cut -d "," -f 3,4 $1 | sed -n '/^12/p'| cut -d "," -f 2| sort -n| head -1)
+
+#usage: echo "string_of_characters_to_print "
+#usage: echo "variable1-variable2_print_results" | bc
+echo "Difference in minimum wages due to graduating:"
+echo "$min16 - $min12"| bc
+
+#usage: variable=$(cut -d delimiter -f columns_to_keep filename|sed -n '/keepline_starting_with_number/p' |cut -d delimiter -f column_to_keep |
+#awk '{variable_name+=sprintf("%number_type", $column_number)} END{printf "%number_type\newline", variable/NR}') 
+avg16=$(cut -d "," -f 3,4 $1 | sed -n '/^16/p'| cut -d "," -f 2| awk '{sum+=sprintf("%f",$1)}END{printf "%.6f\n",sum/NR}')
+avg12=$(cut -d "," -f 3,4 $1 | sed -n '/^12/p'| cut -d "," -f 2| awk '{sum+=sprintf("%f",$1)}END{printf "%.6f\n",sum/NR}')
 
 #usage: echo "string_of_characters_to_print "
 #usage: echo "variable1-variable2_print_results" | bc
 echo "Graduating from college shows hire earning wages by an average of " 
-echo "$val16-$val12" | bc
+echo "$avg16-$avg12" | bc
